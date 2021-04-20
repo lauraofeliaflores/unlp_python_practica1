7e34a254512737146af5557380cde294001df45e
lauraofeliaflores <lauraofeliaflores@gmail.com>
2021-04-20 12:05:59 -0300

    se agrega la descarga de la imágen

--- actividad_20_04_2021.py
+++ actividad_20_04_2021.py
@@ -1,4 +1,5 @@
 import csv
+import requests
 archivo = open("C:/Proyectos/proyectosPython/archivosPY/appstore_games.csv", "r")
 csvreader = csv.reader(archivo, delimiter=',')
 items = list(csvreader)
@@ -19,4 +20,7 @@ items.sort(reverse=True, key=lambda elem: tryconvert(elem[6], 0, int))
 
 for x in range(0,9):
   print(items[x][4])
-  
+  icono = requests.get(items[x][4])
+  juego = items[x][1]
+  with open(f'C:/Proyectos/proyectosPython/archivosPY/ejemplo/clase6/{juego}.jpg', 'wb') as f:
+    f.write(icono.content)
